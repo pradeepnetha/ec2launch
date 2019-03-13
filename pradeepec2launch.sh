a46f1f3d02739d6822fe3043875b776d25dc17dd
pradeepnetha <38692979+pradeepnetha@users.noreply.github.com>
2019-03-13 17:44:36 +0530

    Update pradeepec2launch.sh

--- pradeepec2launch.sh
+++ pradeepec2launch.sh
@@ -12,18 +12,18 @@
 #read region_name
 
 #img_id=ami-0b500ef59d8335eee
-instance_type=t2.micro
-key_name=asg-new
-sg_name=sg-04a6d324940433647
-sub_id=subnet-0e7e366cb34aca9b2
-region_name=us-east-2
+#instance_type=t2.micro
+#key_name=asg-new
+#sg_name=sg-04a6d324940433647
+#sub_id=subnet-0e7e366cb34aca9b2
+#region_name=us-east-2
 
-#img_id=$1
-#instance_type=$2
-#key_name=$3
-#sg_name=$4
-#sub_id=$5
-#region_name=$6
+img_id=$1
+instance_type=$2
+sub_id=$3
+region_name=$4
+sg_name=$5
+key_name=$6
 
 
 #aws ec2 run-instances --image-id ami-abc12345 --count 1 --instance-type t2.micro --key-name MyKeyPair --subnet-id subnet-6e7f829e --tag-specifications 'ResourceType=instance,Tags=[{Key=webserver,Value=production}]' 'ResourceType=volume,Tags=[{Key=cost-center,Value=cc123}]' 
